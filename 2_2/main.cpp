e06279976a092b1b654babe8fe9d827b17c680d8
IlyaUsmanov <60606315+IlyaUsmanov@users.noreply.github.com>
2022-10-31 00:57:10 +0600

    Update main.cpp

--- 2_2/main.cpp
+++ 2_2/main.cpp
@@ -5,37 +5,26 @@ public:
         for (auto num: nums) {
             counter[num]++;
         }
-        vector<int> unique_nums;
-        vector<int> unique_nums_counts;
-        for (auto kv: counter) {
-            unique_nums.push_back(kv.first);
-            unique_nums_counts.push_back(kv.second);
-        }
+        vector<vector<int>> subsets;
         vector<int> cur;
-        vector<vector<int>> unique_counts;
-        idsSubsets(unique_counts, unique_nums_counts, cur);
-        vector<vector<int>> res;
-        for (int i = 0; i < unique_counts.size(); i++) {
-            vector<int> cur_res;
-            for (int j = 0; j < unique_nums.size(); j++) {
-                for (int k = 0; k < unique_counts[i][j]; k++) {
-                    cur_res.push_back(unique_nums[j]);
-                }
-            }
-            res.push_back(cur_res);
-        }
-        return res;
+        
+        idsSubsets(subsets, counter.begin(), counter.end(), cur);
+        
+        return subsets;
     }
 private:
-    void idsSubsets(vector<vector<int>>& unique_counts, vector<int>& ids, vector<int>& cur) {
-        if (ids.size() == cur.size()) {
-            unique_counts.push_back(cur);
+    void idsSubsets(vector<vector<int>>& subsets, map<int, int>::iterator counter_it, map<int, int>::iterator counter_end, vector<int>& cur) {
+        if (counter_it == counter_end) {
+            subsets.push_back(cur);
             return;
         }
-        int cur_id = cur.size();
-        for (int i = 0; i <= ids[cur_id]; i++) {
-            cur.push_back(i);
-            idsSubsets(unique_counts, ids, cur);
+        auto [num, count] = *counter_it;
+        counter_it++;
+        for (int i = 0; i <= count; i++) {
+            idsSubsets(subsets, counter_it, counter_end, cur);
+            cur.push_back(num);
+        }
+        for (int _ = 0; _ <= count; _++) {
             cur.pop_back();
         }
     }
